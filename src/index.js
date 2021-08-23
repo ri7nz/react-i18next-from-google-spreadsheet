d7303586deba7548736763ef4f6db7e9a8108a22
ri7nz <ri7nz@evilfactory.id>
2021-08-24 03:39:10 +0800

    feat(google-api): basic feature works with ApiKey Authorization
    example:
    npx @ri7nz/react-i18next-from-google-spreadsheet  --key  --location ./public/ --firstrow 1 --apiKey

--- src/index.js
+++ src/index.js
@@ -1,38 +1,62 @@
-const loader = require('./loader')
-var argv = require('minimist')(process.argv.slice(2))
+const loader = require("./loader");
+var argv = require("minimist")(process.argv.slice(2));
 
 //arguments from terminal
-let foundKey = argv.key
-let foundLocation = argv.location
-let foundRowNumber = argv.firstrow
+let foundKey = argv.key;
+let foundLocation = argv.location;
+let foundRowNumber = argv.firstrow;
+let foundApiKey = argv.apiKey;
 
-if ((typeof foundKey !== 'string' && typeof foundKey !== 'number') || foundKey == '') {
-  console.error('Key is required: --key=... ')
-  process.exit(1)
+if (
+  (typeof foundKey !== "string" && typeof foundKey !== "number") ||
+  foundKey == ""
+) {
+  console.error("Key is required: --key=... ");
+  process.exit(1);
 }
-if ((typeof foundLocation !== 'string' && typeof foundLocation !== 'number') || foundLocation == '') {
-  console.error('Location is required: --location=... ')
-  process.exit(1)
+if (
+  (typeof foundLocation !== "string" && typeof foundLocation !== "number") ||
+  foundLocation == ""
+) {
+  console.error("Location is required: --location=... ");
+  process.exit(1);
 }
-if (typeof foundRowNumber !== 'number' || foundRowNumber == '') {
-  console.error('First row is required: --firstrow=... ')
-  process.exit(1)
+if (typeof foundRowNumber !== "number" || foundRowNumber == "") {
+  console.error("First row is required: --firstrow=... ");
+  process.exit(1);
+}
+if (typeof foundApiKey !== "string" || foundApiKey == "") {
+  console.error(`GoogleSpreadsheet minimum authorization is ApiKey
+
+see https://theoephraim.github.io/node-google-spreadsheet/#/getting-started/authentication?id=api-key`);
+  process.exit(1);
 }
 
-const loadOptions = { docId: foundKey, sheet: 1, firstRow: foundRowNumber, ignoreCommentsColumn: true }
-const saveOptions = { folderName: foundLocation }
+const loadOptions = {
+  docId: foundKey,
+  sheet: 1,
+  firstRow: foundRowNumber,
+  apiKey: foundApiKey,
+  ignoreCommentsColumn: true,
+};
 
+const saveOptions = { folderName: foundLocation };
+
+console.info("Loading...");
 loader.load(loadOptions, (err, data) => {
+  console.info("Loaded!", { data });
+  console.info("Saving...");
   if (err) {
-    console.error(err)
-    process.exit(1)
+    console.error(err);
+    process.exit(1);
   }
   //console.log('-', data)
   try {
-    loader.save(saveOptions, data)
+    loader.save(saveOptions, data);
   } catch (e) {
-    console.error(e)
-    process.exit(1)
+    console.error(e);
+    process.exit(1);
+  } finally {
+    console.log("!Done!");
   }
-  console.log('!Done!')
-})
+});
--- src/loader.js
+++ src/loader.js
@@ -1,115 +1,78 @@
-const GoogleSpreadsheet = require('google-spreadsheet')
-const fs = require('fs')
-const TRANSLATION_FILE = 'translation.json'
-
+const GoogleSpreadsheet = require("google-spreadsheet").GoogleSpreadsheet;
+const fs = require("fs");
+const TRANSLATION_FILE = "translation.json";
+/**
+ * This is only work when  your table like this
+ * ```markdown
+ * | key | langCode1 | langCode2
+ * | x   | x1        | x2
+ * ```
+ */
 const load = (options, cb) => {
-  const my_sheet = new GoogleSpreadsheet(options.docId)
-  my_sheet.getInfo(function(err, info) {
-    if (err) {
-      return cb('invalid google key')
-    }
-
-    const sheet = info.worksheets[options.sheet - 1]
-    const firstRow = options.firstRow || 1
-    const colCount = options.colCount || sheet.colCount
-    const rowCount = sheet.rowCount
-
-    my_sheet.getCells(
-      options.sheet,
-      {
-        'min-row': firstRow,
-        'max-row': rowCount,
-        'min-col': 1,
-        'max-col': colCount,
-        'return-empty': true,
-      },
-      function(err, row_data) {
-        if (err) {
-          return cb(err)
-        }
-        const converted = {}
-        const langs = []
-        let commentsColumnIndex
-
-        for (let i = 1; i < colCount; i++) {
-          if (options.ignoreCommentsColumn == true && row_data[i].value == 'comments') {
-            // do nothing
-            commentsColumnIndex = i
-          } else {
-            if (options.warnOnMissingValues && row_data[i].value.length == 0) {
-              console.log('Column is missing key at index ' + i)
+  const my_sheet = new GoogleSpreadsheet(options.docId);
+  if (options.apiKey) {
+    console.info("Authorization use ApiKey");
+    my_sheet.useApiKey(options.apiKey).then(() => {
+      my_sheet.loadInfo().then(() => {
+        const sheet = my_sheet.sheetsByIndex[options.sheet - 1];
+        sheet.getRows().then((rows) => {
+          let data = rows.reduce((_converted, row) => {
+            // console.log({
+            //   row,
+            //   keys: Object.keys(row),
+            //   headerValues: row._sheet.headerValues,
+            // });
+            const [_key, ...headers] = row._sheet.headerValues || [];
+            if (headers.length <= 0) {
+              return cb("empty language code in header of your spreadsheet");
             }
-            if (options.errorOnMissingValues && row_data[i].value.length == 0) {
-              throw new Error('Column is missing key at index ' + i)
-            }
-            if (row_data[i].value !== undefined) {
-              langs[i] = row_data[i].value
-            }
-          }
-        }
-
-        for (let i = firstRow + 1; i <= rowCount; i++) {
-          for (let j = 1; j < colCount; j++) {
-            if (options.ignoreCommentsColumn && j == commentsColumnIndex) {
-              // do nothing
-            } else {
-              if (options.warnOnMissingValues && row_data[(i - 1) * colCount + j].value.length == 0) {
-                console.log('Cell is missing value at col ' + i + ', row ' + j)
-              }
-              if (options.errorOnMissingValues && row_data[(i - 1) * colCount + j].value.length == 0) {
-                throw new Error('Cell is missing value at col ' + i + ', row ' + j)
-              }
-              let lang = langs[j]
-
-              converted[lang] = converted[lang] || {}
-
-              if (row_data[(i - 1) * colCount + j] === undefined) {
-                continue
-              }
-              if (row_data[(i - 1) * colCount + j].value && row_data[(i - 1) * colCount + j].value !== undefined) {
-                converted[lang][row_data[(i - 1) * colCount].value] = row_data[(i - 1) * colCount + j].value
+            let key = row[_key];
+            headers.forEach((lang) => {
+              if (_converted.hasOwnProperty(lang)) {
+                _converted[lang][key] = row[lang];
               } else {
+                _converted[lang] = { [key]: row[lang] };
               }
-            }
-          }
-        }
-
-        return cb(null, converted)
-      }
-    )
-  })
-}
+            });
+            return _converted;
+          }, {});
+          cb(null, data);
+        });
+      });
+    });
+  }
+};
 
 const save = (options, data) => {
-  const folderName = options.folderName
+  const folderName = options.folderName;
 
   try {
     if (!fs.existsSync(folderName)) {
-      fs.mkdirSync(folderName)
+      fs.mkdirSync(folderName);
     }
   } catch (e) {
-    console.log(`Invalid 'location' parameter: ${folderName}`)
-    throw `Invalid 'location' parameter: ${folderName}`
+    console.log(`Invalid 'location' parameter: ${folderName}`);
+    throw `Invalid 'location' parameter: ${folderName}`;
   }
 
-  Object.keys(data).forEach(lang => {
+  Object.keys(data).forEach((lang) => {
     try {
-      fs.existsSync(`${folderName}/${lang}`)
-    } catch(e) {
-      console.log(`Invalid 'language' name: ${lang}`, e)
-      throw `Invalid 'language' name: ${lang}`
+      fs.existsSync(`${folderName}/${lang}`);
+    } catch (e) {
+      console.log(`Invalid 'language' name: ${lang}`, e);
+      throw `Invalid 'language' name: ${lang}`;
     }
     if (!fs.existsSync(`${folderName}/${lang}`)) {
       try {
-        fs.mkdirSync(`${folderName}/${lang}`)
-      } catch(e) {
-        console.log(`Invalid 'language' name: ${lang}`, e)
-        throw `Invalid 'language' name: ${lang}`
+        fs.mkdirSync(`${folderName}/${lang}`);
+      } catch (e) {
+        console.log(`Invalid 'language' name: ${lang}`, e);
+        throw `Invalid 'language' name: ${lang}`;
       }
     }
-    const content = JSON.stringify(data[lang], null, '\t')
-    fs.writeFileSync(`${folderName}/${lang}/${TRANSLATION_FILE}`, content)
-  })
-}
+    const content = JSON.stringify(data[lang], null, "\t");
+    fs.writeFileSync(`${folderName}/${lang}/${TRANSLATION_FILE}`, content);
+  });
+};
 
-module.exports = { load, save }
+module.exports = { load, save };
